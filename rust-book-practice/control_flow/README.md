a7051cbdcf7e62831b82f74e9ec7fb145fccb408
shaqsnake <shaqsnake@gmail.com>
2019-09-30 17:35:06 +0800

    Add new project 'control_flow'.

---
+++ rust-book-practice/control_flow/README.md
@@ -0,0 +1,8 @@
+<!--
+ * @Author: shaqsnake
+ * @Email: shaqsnake@gmail.com
+ * @Date: 2019-09-30 16:36:50
+ * @LastEditTime: 2019-09-30 17:19:42
+ * @Description: The Rust Programming Language - Ch3.3 Functions
+ -->
+[The Rust Programming Language - Ch3.5 Control Flow](https://doc.rust-lang.org/book/ch03-05-control-flow.html)
---
+++ rust-book-practice/control_flow/src/main.rs
@@ -0,0 +1,82 @@
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition {
+        5
+    } else {
+        6
+    };
+
+    println!("The value of number is: {}", number);
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+        
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+
+    for i in 0..11 {
+        println!("The fib({}) = {}", i, fib(i));
+    }
+}
+
+fn fib(x: u32) -> u32 {
+    if x < 2 {
+        return x;
+    }
+
+    (fib(x - 1) + fib(x - 2))
+}
