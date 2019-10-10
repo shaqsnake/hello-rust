27bfa30e4dfac8f34bf813772c45ea971a1a2cd5
shaqsnake <shaqsnake@gmail.com>
2019-10-10 16:38:37 +0800

    Add new project "collections".

---
+++ rust-book-practice/collecitons/src/main.rs
@@ -0,0 +1,47 @@
+fn main() {
+    // Vectors
+    let v: Vec<i32> = Vec::new();
+    let v = vec![1, 2, 3];
+    println!("{:?}", v);
+
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    println!("{:?}", v);
+
+    let v = vec![1, 2, 3, 4, 5];
+    
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(3) {
+        Some(i) => println!("The third element is {}", i),
+        None => println!("There is no third element."),
+    }
+
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+    println!("{:?}", v);
+
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+    println!("{:?}", row);
+}
