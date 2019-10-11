bf05e418fae21f927d166e8689383080896e2b0f
shaqsnake <shaqsnake@gmail.com>
2019-10-11 11:17:45 +0800

    Update project "collections".

--- rust-book-practice/collecitons/src/main.rs
+++ rust-book-practice/collecitons/src/main.rs
@@ -121,4 +121,49 @@ fn main() {
     for b in hello.bytes() {
         println!("{}", b);
     }
+
+    // Hash maps
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+    println!("{:?}", scores);
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        println!("{}", word);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
