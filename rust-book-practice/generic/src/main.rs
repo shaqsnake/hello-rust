cc65fa8675499308e7dde123d24ca9a2b0bde5e6
shaqsnake <shaqsnake@gmail.com>
2019-10-12 17:18:58 +0800

    Update project "generic".

--- rust-book-practice/generic/src/main.rs
+++ rust-book-practice/generic/src/main.rs
@@ -1,14 +1,27 @@
-// fn largest<T>(list: &[T]) -> T {
-//     let mut largest = list[0];
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
 
-//     for &item in list.iter() {
-//         if item > largest {
-//             largest = item
-//         }
-//     }
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
 
-//     largest
-// }
+    largest
+}
+
+fn largest2<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest_idx = 0;
+
+    for i in 0..list.len() {
+        if list[i] > list[largest_idx] {
+            largest_idx = i
+        }
+    }
+
+    &list[largest_idx]
+}
+
 
 struct Point<T, U> {
     x: T,
@@ -25,20 +38,21 @@ impl<T, U> Point<T, U> {
 }
 
 fn main() {
-    // let number_list = vec![34, 50, 25, 100, 65];
+    let number_list = vec![34, 50, 25, 100, 65];
 
-    // let result = largest(&number_list);
-    // println!("The largest number is {}", result);
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
-    // let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-    // let result = largest(&number_list);
-    // println!("The largest number is {}", result);
+    let result = largest2(&number_list);
+    println!("The largest number is {}", result);
 
-    // let char_list = vec!['y', 'm', 'a', 'q'];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    // let result = largest(&char_list);
-    // println!("The largest char is {}", result);
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
     let p1 = Point { x: 5, y: 10.4};
     let p2 = Point { x: "Hello", y: 'c'};
 
