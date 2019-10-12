b9351f64bde6894b5cf6793e140d72e3972cda79
shaqsnake <shaqsnake@gmail.com>
2019-10-12 15:33:18 +0800

    Add new project "generic".

---
+++ rust-book-practice/generic/src/main.rs
@@ -0,0 +1,48 @@
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item
+//         }
+//     }
+
+//     largest
+// }
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    // let number_list = vec![34, 50, 25, 100, 65];
+
+    // let result = largest(&number_list);
+    // println!("The largest number is {}", result);
+
+    // let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    // let result = largest(&number_list);
+    // println!("The largest number is {}", result);
+
+    // let char_list = vec!['y', 'm', 'a', 'q'];
+
+    // let result = largest(&char_list);
+    // println!("The largest char is {}", result);
+    let p1 = Point { x: 5, y: 10.4};
+    let p2 = Point { x: "Hello", y: 'c'};
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
