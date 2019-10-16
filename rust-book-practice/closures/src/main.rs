7a274f958dda6c47355eecb9d12540b711854024
shaqsnake <shaqsnake@gmail.com>
2019-10-16 15:31:55 +0800

    Update project "closures".

--- rust-book-practice/closures/src/main.rs
+++ rust-book-practice/closures/src/main.rs
@@ -70,6 +70,24 @@ fn main() {
         simulated_user_specified_value,
         simulated_user_random_number
     );
+
+    let x = 4;
+
+    let equal_to_x = |z| z== x;
+
+    let y = 4;
+
+    assert!(equal_to_x(y));
+
+    let x = vec![1, 2, 3];
+
+    let equal_to_x = move |z| z == x;
+
+    // println!("can't use x here: {:?}", x);
+
+    let y = vec![1, 2, 3];
+
+    assert!(equal_to_x(y));
 }
 
 #[test]
