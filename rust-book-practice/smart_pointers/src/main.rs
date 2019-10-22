d0808d9adb1a72541f7a6311c15e5aee9dd027a9
shaqsnake <shaqsnake@gmail.com>
2019-10-22 09:22:13 +0800

    Update some drop examples.

--- rust-book-practice/smart_pointers/src/main.rs
+++ rust-book-practice/smart_pointers/src/main.rs
@@ -28,6 +28,16 @@ fn hello(name: &str) {
     println!("Hello, {}!", name);
 }
 
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
@@ -58,4 +68,10 @@ fn main() {
 
     let m = MyBox::new(String::from("Rust"));
     hello(&m);
+
+    let c = CustomSmartPointer { data: String::from("my stuff") };
+    let d = CustomSmartPointer { data: String::from("other stuff")};
+    println!("CustomSmartPointers created.");
+    drop(c);
+    println!("CustomSmartPointers dropped before the end of main.");
 }
