9f8e6adfb31a330cc6f87afcccbbacf4afa9efc8
shaqsnake <shaqsnake@gmail.com>
2019-10-23 14:30:11 +0800

    Update to Weak<T> for preventing reference cycles.

--- rust-book-practice/ref_cycles/src/main.rs
+++ rust-book-practice/ref_cycles/src/main.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 use std::cell::RefCell;
 use crate::List::{Cons, Nil};
 
@@ -20,6 +20,7 @@ impl List {
 #[derive(Debug)]
 struct Node {
     value: i32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
 
@@ -44,21 +45,38 @@ fn main() {
     // it will overflow the stack
     // println!("a next item = {:?}", a.tail());
 
+    // let leaf = Rc::new(Node {
+    //     value: 3,
+    //     children: RefCell::new(vec![]),
+    // });
+
+    // let leaf2 = Rc::new(Node {
+    //     value: 4,
+    //     children: RefCell::new(vec![]),
+    // });
+
+    // let branch = Rc::new(Node {
+    //     value: 5,
+    //     children: RefCell::new(vec![Rc::clone(&leaf), Rc::clone(&leaf2)]),
+    // });
+
+    // println!("{:?}", branch.children);
+    // println!("leaf rc count after link to branch = {}", Rc::strong_count(&leaf));
+
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
-
-    let leaf2 = Rc::new(Node {
-        value: 4,
-        children: RefCell::new(vec![]),
-    });
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 
     let branch = Rc::new(Node {
         value: 5,
-        children: RefCell::new(vec![Rc::clone(&leaf), Rc::clone(&leaf2)]),
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
 
-    println!("{:?}", branch.children);
-    println!("leaf rc count after link to branch = {}", Rc::strong_count(&leaf));
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+    println!("{:?}", *leaf);
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }
