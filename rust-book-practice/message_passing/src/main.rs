4e61576454038ef263b962c93a1786975328fcd8
shaqsnake <shaqsnake@gmail.com>
2019-10-26 16:00:46 +0800

    Update "message_passing", add more concurrency examples.

--- rust-book-practice/message_passing/src/main.rs
+++ rust-book-practice/message_passing/src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::sync::mpsc;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
@@ -11,4 +12,62 @@ fn main() {
 
     let received = rx.recv().unwrap();
     println!("Got: {}", received);
+
+    // ------
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+    println!("------");
+
+    // ------
+    let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        } 
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
