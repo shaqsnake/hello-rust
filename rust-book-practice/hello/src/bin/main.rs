b6aa172b8f47622877f318cb6246e0ce911bdeb0
shaqsnake <shaqsnake@gmail.com>
2019-12-06 10:27:52 +0800

    Graceful Shutdown and Cleanup.

--- rust-book-practice/hello/src/bin/main.rs
+++ rust-book-practice/hello/src/bin/main.rs
@@ -4,19 +4,22 @@ use std::net::TcpListener;
 use std::fs;
 use std::thread;
 use std::time::Duration;
+
 use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
